da46ff7eaf281ae7d197112ad5b69517f236e11a
ali.sorouramini <ali.sorouramini@gmail.com>
2023-06-30 13:11:09 +0330

    Fix vim packer installer

--- install.bash
+++ install.bash
@@ -14,6 +14,6 @@ pyenv global 3.11
 brew install tmux
 
 brew install neovim
-git clone https://github.com/wbthomason/packer.nvim "$env:LOCALAPPDATA\nvim-data\site\pack\packer\start\packer.nvim"
+git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
 source config/install.bash
