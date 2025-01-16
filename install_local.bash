68444fae3c9461d748d5ee63191294625ce1303f
ali.sorouramini <ali.sorouramini@gmail.com>
2025-01-16 17:23:07 +0330

    Fix configs

--- install_local.bash
+++ install_local.bash
@@ -8,8 +8,6 @@ eval "$(pyenv init -)"
 pyenv install -s 3.12
 pyenv global 3.12
 
-source install_ssh_keys.bash
-
 pushd config
 source install.bash
 popd
--- install_ssh_keys.bash
+++
@@ -1,5 +0,0 @@
-
-curl -L \
-  -H "Accept: application/vnd.github+json" \
-  -H "X-GitHub-Api-Version: 2022-11-28" \
-  https://api.github.com/users/alisoam/keys | jq -r ".[] | .key + \" \" + (.id |tostring)" >~/.ssh/authorized_keys
