7309f5fce6a49fe351c8db1abcc193275eb3a5de
ali.sorouramini <ali.sorouramini@gmail.com>
2025-03-06 13:33:37 +0330

    Update config

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -103,7 +103,77 @@ require("lazy").setup({
   -- {
   --   'Exafunction/codeium.vim',
   --   event = 'BufEnter',
-  -- }
+  -- },
+  {
+    "yetone/avante.nvim",
+    event = "VeryLazy",
+    lazy = false,
+    version = "*",
+    opts = {
+      --provider = "copilot",
+      -- auto_suggestions_provider = "copilot",
+      -- cursor_applying_provider = "copilot",
+      provider = "openai",
+      openai = {
+        endpoint = os.getenv("OPENAI_API_BASE"),
+        model = "gpt-4o", -- your desired model (or use gpt-4o, etc.)
+        timeout = 10000, -- timeout in milliseconds
+        temperature = 0, -- adjust if needed
+        max_tokens = 4096,
+        -- reasoning_effort = "high" -- only supported for reasoning models (o1, etc.)
+      }
+    },
+    build = "make",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "stevearc/dressing.nvim",
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      --- The below dependencies are optional,
+      "echasnovski/mini.pick", -- for file_selector provider mini.pick
+      "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
+      "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
+      "ibhagwan/fzf-lua", -- for file_selector provider fzf
+      "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+      "zbirenbaum/copilot.lua", -- for providers='copilot'
+      {
+        -- support for image pasting
+        "HakonHarnes/img-clip.nvim",
+        event = "VeryLazy",
+        opts = {
+          -- recommended settings
+          default = {
+            embed_image_as_base64 = false,
+            prompt_for_file_name = false,
+            drag_and_drop = {
+              insert_mode = true,
+            },
+            -- required for Windows users
+            use_absolute_path = true,
+          },
+        },
+      },
+      {
+        -- Make sure to set this up properly if you have lazy=true
+        'MeanderingProgrammer/render-markdown.nvim',
+        opts = {
+          file_types = { "markdown", "Avante" },
+        },
+        ft = { "markdown", "Avante" },
+      },
+    },
+  },
+  -- {
+  --   "OXY2DEV/markview.nvim",
+  --   enabled = true,
+  --   lazy = false,
+  --   ft = { "markdown", "norg", "rmd", "org", "vimwiki", "Avante" },
+  --   opts = {
+  --     filetypes = { "markdown", "norg", "rmd", "org", "vimwiki", "Avante" },
+  --     buf_ignore = {},
+  --     max_length = 99999,
+  --   },
+  -- },
 })
 
 vim.opt.autoindent = true
