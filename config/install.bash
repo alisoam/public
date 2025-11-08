ccde60a6c3eff5b238fd62f2d8c603fcccd43b50
ali.sorouramini <ali.sorouramini@gmail.com>
2025-11-08 21:48:39 +0330

    Remove pyenv

--- config/install.bash
+++ config/install.bash
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-ENV=$( ( [ "command -v pyenv" ] && echo PYENV_VERSION=system pyenv exec ) || echo )
-eval $ENV python3 sync.py sync_host
+/usr/bin/python3 sync.py sync_host
--- install_local.bash
+++ install_local.bash
@@ -4,10 +4,6 @@ source install_apt.bash
 
 source install_brew.bash
 
-eval "$(pyenv init -)"
-pyenv install -s 3.12
-pyenv global 3.12
-
 pushd config
 source install.bash
 popd
