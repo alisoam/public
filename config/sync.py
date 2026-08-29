63bfbca3994c23fe39d6dea3b57ddae85dc61756
ali.sorouramini <ali.sorouramini@gmail.com>
2026-08-29 14:37:37 +0330

    update configs

--- config/sync.py
+++ config/sync.py
@@ -181,9 +181,6 @@ all_configs: list[Config] = [
     Directory("kitty", "kitty", "~/.config/kitty"),
 
     Directory("opencode", "opencode", "~/.config/opencode"),
-
-    File("i3", "i3/i3", "~/.config/i3/config"),
-    File("i3", "i3/i3status", "~/.config/i3status/config"),
 ]
 
 
